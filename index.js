8fa8821d493d67b0bf3d7ede3d32eee9ae0ee980
Abheeshta-P <softwarecheck01+github@gmail.com>
2024-12-08 20:12:28 +0530

    quick changes

--- index.js
+++ index.js
@@ -121,5 +121,5 @@ app.post('/chat', async (req, res) => {
 });
 
 // Start Server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
